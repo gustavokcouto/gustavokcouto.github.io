9bd48ff473c04608e39801fc140605711501188e
Gustavo Claudio Karl Couto <gustavokcouto@uol.com.br>
2016-06-05 20:45:03 -0300

    modificando o formulário

--- js/roboime.js
+++ js/roboime.js
@@ -1,7 +1,7 @@
 $(document).ready(
   function() {
     var contactForm = $('#contact-form');
-    $("button#submit").click(function(){
+    $("button#contact_submit").click(function(){
       $.ajax({
         url: contactForm.attr('action'),
         method: contactForm.attr('method'),
@@ -27,7 +27,37 @@ $(document).ready(
           errorBox.fadeTo(2000, 500).slideUp(700, function() {
             $(".alert-danger").alert('close');
           });
-        });
-      }
+        },
+      });
+    });
+    var participeForm = $('#participe-form');
+    $("button#participe_submit").click(function(){
+      $.ajax({
+        url: participeForm.attr('action'),
+        method: participeForm.attr('method'),
+        data: participeForm.serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          participeForm.append('<div class="alert-loading" >Sending message<85></div>');
+        },
+        success: function(data) {
+          participe.find('.alert-loading').remove();
+          var succesBox = $('<div class="alert alert-success alert-dismissible" role="alert">' + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '<strong>Thanks!</strong> I will reply in a few hours' + '</div>').hide();
+          participeForm.before(succesBox);
+          succesBox.fadeIn("slow");
+          succesBox.fadeTo(2000, 500).slideUp(500, function() {
+            $(".alert-success").alert('close');
+          });
+        },
+        error: function(err) {
+          participeForm.find('.alert-loading').remove();
+          var errorBox = $('<div class="alert alert-danger alert-dismissible" role="alert">' + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '<strong>Error!</strong> Something went wrong' + '</div>').hide();
+          participeForm.before(errorBox);
+          errorBox.fadeIn("slow");
+          errorBox.fadeTo(2000, 500).slideUp(700, function() {
+            $(".alert-danger").alert('close');
+          });
+        },
+      });
     });
 });
